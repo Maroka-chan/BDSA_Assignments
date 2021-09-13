27bd4255ab26072a6ee9b1e0d5c7553e212237c2
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2021-09-13 18:51:58 +0200

    Add Null test for Flatten

--- Assignment.Tests/IteratorsTests.cs
+++ Assignment.Tests/IteratorsTests.cs
@@ -7,31 +7,29 @@ namespace Assignment.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Flatten_GivenStreamOfStreams_ReturnsJoinedStream()
-        {
+        public void Flatten_GivenStreamOfStreams_ReturnsJoinedStream() {
             // Arrange
             var stream = new[] { new[] { 2 }, new[] { 2 } };
             var expected = new[] { 2, 2 };
-
-            // Act
-            var resultStream = Iterators.Flatten(stream);
-
-            // Assert
-            Assert.Equal(expected, resultStream);
+            
+            var resultStream = Iterators.Flatten(stream); // Act
+            Assert.Equal(expected, resultStream); // Assert
         }
         
         [Fact]
-        public void Flatten_GivenEmptyStream_ReturnsEmptyStream()
-        {
+        public void Flatten_GivenEmptyStream_ReturnsEmptyStream() {
             // Arrange
-            var stream = new IEnumerable<int>[1];
+            var stream = new int[1][];
             var expected = new int[1];
-
-            // Act
-            var resultStream = Iterators.Flatten(stream);
-
-            // Assert
-            Assert.Equal(expected, resultStream);
+            
+            var resultStream = Iterators.Flatten(stream); // Act
+            Assert.Equal(expected, resultStream); // Assert
+        }
+        
+        [Fact]
+        public void Flatten_GivenNull_ThrowsArgumentNullException() {
+            IEnumerable<IEnumerable<int>> stream = null; // Arrange
+            Assert.Throws<ArgumentNullException>(() => Iterators.Flatten(stream)); // Assert
         }
     }
 }
