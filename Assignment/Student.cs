0983ef0292a104bec07135642152b7e4ae5c3c3d
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2021-09-19 16:59:03 +0200

    Implement Status

--- Assignment/Student.cs
+++ Assignment/Student.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 
 namespace Assignment
 {
@@ -7,7 +8,26 @@ namespace Assignment
         public int Id { get; init; }
         public string GivenName { get; set; }
         public string Surname { get; set; }
-        public Status Status { get; }
+
+        public Status Status
+        {
+            get
+            {
+                var currentDate = DateTime.Now;
+                var startDate = StartDate;
+                var endDate = EndDate;
+                if (endDate < startDate) throw new InvalidConstraintException("End Date can't be before Start Date.");
+                if (currentDate < startDate) throw new InvalidConstraintException("Student haven't enrolled yet.");
+                var twoMonthsAfterStart = StartDate.AddMonths(2);
+                if (currentDate <= twoMonthsAfterStart) return Status.New;
+                var graduationDate = GraduationDate;
+                if (currentDate > graduationDate && endDate > graduationDate) return Status.Graduated;
+                if (currentDate > twoMonthsAfterStart && currentDate < graduationDate) return Status.Active;
+                if (endDate < GraduationDate) return Status.Dropout;
+                throw new InvalidOperationException("Status couldn't be determined.");
+            }
+        }
+
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public DateTime GraduationDate { get; set; }
