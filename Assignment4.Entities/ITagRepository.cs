1a7314ce7972b783daa1ccbf0e41fc25347b7219
Carl <carl99@live.dk>
2021-10-07 14:13:03 +0200

    A first look at ITagRepository

---
+++ Assignment4.Entities/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment4.Entities
+{
+    public interface ITagRepository : IDisposable
+    {
+        int Create(Tag tag); //should return the id of the new tag
+        Tag read(int tagId); 
+        void Update(Tag tag);
+        void delete(int tagId);
+    }
+}
