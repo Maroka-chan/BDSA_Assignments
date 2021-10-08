ca796c2ae6d9eadd561cea0abaa9f98db96fb901
Carl <carl99@live.dk>
2021-10-08 13:26:13 +0200

    initial TagRepo

---
+++ Assignment4.Entities/TagRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment4.Entities
+{
+    public class TagRepository : ITagRepository
+    {
+        public int Create(Tag tag)
+        {
+            return 1;
+        }
+
+        public Tag read(int tagId)
+        {
+            return null;
+        }
+
+        public void Update(Tag tag)
+        {
+        }
+
+        public void delete(int tagId)
+        {
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
